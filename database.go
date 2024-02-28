bd64094f7fa537c270020daf03a0dc0f1ea1cf5b
Sebastian Ibanez <91081658+sebastian-j-ibanez@users.noreply.github.com>
2024-02-27 22:48:33 -0500

    Start fancy list implementation. Use dbItemHandler.

--- database.go
+++ database.go
@@ -3,6 +3,9 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"math/rand"
+	"os"
+	"sync"
 
 	_ "github.com/mattn/go-sqlite3"
 )
@@ -11,32 +14,62 @@ import (
 const DB_FILE = "vault.db"
 
 // Struct to represent the quote table.
-type Quote struct {
-	ID     int
-	Body   string
-	Author string
-	Date   string
+type databaseItemHandler struct {
+	database  *sql.DB
+	quotes    []item
+	quoteIds  []int
+	listIndex int
+	mtx       *sync.Mutex
+	shuffle   *sync.Once
 }
 
 // Initialize a database using the DB_FILE name.
-func InitDb() (*sql.DB, error) {
-	db, err := sql.Open("sqlite3", DB_FILE)
+func (d databaseItemHandler) resetDbItemHandler() error {
+	// Set mtx and shuffle
+	d.mtx = &sync.Mutex{}
+	d.shuffle = &sync.Once{}
+
+	var err error
+
+	// Open database connection
+	d.database, err = sql.Open("sqlite3", DB_FILE)
 	if err != nil {
-		return nil, fmt.Errorf("error: %w", ErrOpenDb)
+		return fmt.Errorf("error: %w", ErrOpenDb)
 	}
 
-	err = CreateQuoteTable(db)
+	// Setup database data (if empty)
+	err = CreateQuoteTable(d.database)
 	if err != nil {
-		return nil, err
+		return err
 	}
 
-	return db, nil
+	// Get quotes from the database
+	// In the format of []item
+	d.quotes, d.quoteIds, err = GetAllQuotes(d.database)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	d.listIndex = 0
+
+	// Shuffle the array of quotes
+	d.shuffle.Do(func() {
+		SpliceShuffle(d.quotes)
+		SpliceShuffle(d.quoteIds)
+	})
+
+	return nil
+}
+
+func SpliceShuffle[T any](s []T) {
+	rand.Shuffle(len(s), func(i, j int) { s[i], s[j] = s[j], s[i] })
 }
 
 // Creates quote table if it does not exist.
 func CreateQuoteTable(db *sql.DB) error {
 	// Create query string.
-	queryString := "CREATE TABLE IF NOT EXISTS quote (id INTEGER PRIMARY KEY, body TEXT NOT NULL, author TEXT, date TEXT)"
+	queryString := "CREATE TABLE IF NOT EXISTS quote (id INTEGER PRIMARY KEY, body TEXT NOT NULL, author TEXT)"
 
 	// Prepare query to create table with the following properties:
 	// id: 		integer primary key
@@ -53,13 +86,67 @@ func CreateQuoteTable(db *sql.DB) error {
 	return nil
 }
 
-// Insert a quote into the database.
-func AddQuote(db *sql.DB, body string, author string, date string) error {
+func GetAllQuotes(db *sql.DB) ([]item, []int, error) {
+
 	// Create query string.
-	queryString := "INSERT INTO quote (body, author, date) VALUES (?, ?, ?)"
+	queryString := "SELECT id, body, author FROM quote"
+
+	// Execute the string.
+	rows, err := db.Query(queryString)
+	if err != nil {
+		return []item{}, []int{}, fmt.Errorf("error: %w", ErrMalformedQuery)
+	}
+
+	// Initialize slice of quotes.
+	var items []item
+	var ids []int
+
+	// Iterate through rows and save to quotes.
+	for rows.Next() {
+		var i item
+		var id int
+		if err := rows.Scan(&id, &i.title, &i.desc); err != nil {
+			return []item{}, []int{}, fmt.Errorf("error: %w", ErrMalformedQuery)
+		}
+		items = append(items, i)
+	}
+
+	// Check if any errors occured while iterating over rows.
+	if err := rows.Err(); err != nil {
+		return []item{}, []int{}, fmt.Errorf("error: %w", ErrRowIteration)
+	}
+
+	return items, ids, nil
+}
+
+func (d databaseItemHandler) GetNextQuote() item {
+	if d.mtx == nil {
+		d.resetDbItemHandler()
+	}
+
+	d.mtx.Lock()
+	defer d.mtx.Unlock()
+
+	i := item{
+		title: d.quotes[d.listIndex].title,
+		desc:  d.quotes[d.listIndex].desc,
+	}
+
+	d.listIndex++
+	if d.listIndex >= len(d.quotes) {
+		d.listIndex = 0
+	}
+
+	return i
+}
+
+// Insert a quote into the database.
+func (d databaseItemHandler) AddQuote(db *sql.DB, i item) error {
+	// Create query string.
+	queryString := "INSERT INTO quote (body, author) VALUES (?, ?)"
 
 	// Construct query using query string and parameters.
-	_, err := db.Exec(queryString, body, author, date)
+	_, err := db.Exec(queryString, i.title, i.desc)
 
 	// Check error.
 	if err != nil {
@@ -70,11 +157,11 @@ func AddQuote(db *sql.DB, body string, author string, date string) error {
 }
 
 // Update a quote entry in the database.
-func UpdateQuote(db *sql.DB, id int, body string, author string, date string) error {
+func (d databaseItemHandler) UpdateQuote(db *sql.DB, i item, id int) error {
 	// Query string to update quote in database.
 	queryString := "UPDATE quote SET body = ?, author = ?, date = ? WHERE id = ?"
 
-	_, err := db.Exec(queryString, body, author, date, id)
+	_, err := db.Exec(queryString, i, id)
 	if err != nil {
 		return fmt.Errorf("error: %w", ErrMalformedQuery)
 	}
@@ -83,7 +170,7 @@ func UpdateQuote(db *sql.DB, id int, body string, author string, date string) er
 }
 
 // Delete a quote from the database given an id.
-func DeleteQuote(db *sql.DB, id int) error {
+func (d databaseItemHandler) DeleteQuote(db *sql.DB, id int) error {
 	// Create query string.
 	queryString := "DELETE FROM quote WHERE id = ?"
 
@@ -96,35 +183,4 @@ func DeleteQuote(db *sql.DB, id int) error {
 	}
 
 	return nil
-}
-
-func GetAllQuotes(db *sql.DB) ([]item, error) {
-
-	// Create query string.
-	queryString := "SELECT body, author FROM quote"
-
-	// Execute the string.
-	rows, err := db.Query(queryString)
-	if err != nil {
-		return []item{}, fmt.Errorf("error: %w", ErrMalformedQuery)
-	}
-
-	// Initialize slice of quotes.
-	var items []item
-
-	// Iterate through rows and save to quotes.
-	for rows.Next() {
-		var i item
-		if err := rows.Scan(&i.title, &i.desc); err != nil {
-			return []item{}, fmt.Errorf("error: %w", ErrMalformedQuery)
-		}
-		items = append(items, i)
-	}
-
-	// Check if any errors occured while iterating over rows.
-	if err := rows.Err(); err != nil {
-		return []item{}, fmt.Errorf("error: %w", ErrRowIteration)
-	}
-
-	return items, nil
 }
--- main.go
+++ main.go
@@ -4,12 +4,24 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/charmbracelet/bubbles/key"
 	"github.com/charmbracelet/bubbles/list"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
 )
 
-var docStyle = lipgloss.NewStyle().Margin(1, 2)
+var (
+	appStyle = lipgloss.NewStyle().Padding(1, 2)
+
+	titleStyle = lipgloss.NewStyle().
+			Foreground(lipgloss.Color("#FFFDF5")).
+			Background(lipgloss.Color("#25A065")).
+			Padding(0, 1)
+
+	statusMessageStyle = lipgloss.NewStyle().
+				Foreground(lipgloss.AdaptiveColor{Light: "#04B575", Dark: "#04B575"}).
+				Render
+)
 
 type item struct {
 	title, desc string
@@ -19,8 +31,88 @@ func (i item) Title() string       { return i.title }
 func (i item) Description() string { return i.desc }
 func (i item) FilterValue() string { return i.title }
 
+type listKeyMap struct {
+	toggleSpinner    key.Binding
+	toggleTitleBar   key.Binding
+	toggleStatusBar  key.Binding
+	togglePagination key.Binding
+	toggleHelpMenu   key.Binding
+	insertItem       key.Binding
+}
+
+func newListKeyMap() *listKeyMap {
+	return &listKeyMap{
+		insertItem: key.NewBinding(
+			key.WithKeys("a"),
+			key.WithHelp("a", "add item"),
+		),
+		toggleSpinner: key.NewBinding(
+			key.WithKeys("s"),
+			key.WithHelp("s", "toggle spinner"),
+		),
+		toggleTitleBar: key.NewBinding(
+			key.WithKeys("T"),
+			key.WithHelp("T", "toggle title"),
+		),
+		toggleStatusBar: key.NewBinding(
+			key.WithKeys("S"),
+			key.WithHelp("S", "toggle status"),
+		),
+		togglePagination: key.NewBinding(
+			key.WithKeys("P"),
+			key.WithHelp("P", "toggle pagination"),
+		),
+		toggleHelpMenu: key.NewBinding(
+			key.WithKeys("H"),
+			key.WithHelp("H", "toggle help"),
+		),
+	}
+}
+
 type model struct {
-	list list.Model
+	list          list.Model
+	dbItemHandler databaseItemHandler
+	keys          *listKeyMap
+	delegateKeys  *delegateKeyMap
+}
+
+func newModel() model {
+	// Initialize model variables
+	var (
+		dbItemHandler databaseItemHandler
+		delegateKeys  = newDelegateKeyMap()
+		listKeys      = newListKeyMap()
+	)
+
+	// Make initial list of items
+	const numItems = 24
+	items := make([]list.Item, numItems)
+	for i := 0; i < numItems; i++ {
+		items[i] = dbItemHandler.GetNextQuote()
+	}
+
+	// Setup list
+	delegate := newItemDelegate(delegateKeys)
+	groceryList := list.New(items, delegate, 0, 0)
+	groceryList.Title = "Groceries"
+	groceryList.Styles.Title = titleStyle
+	groceryList.AdditionalFullHelpKeys = func() []key.Binding {
+		return []key.Binding{
+			listKeys.toggleSpinner,
+			listKeys.insertItem,
+			listKeys.toggleTitleBar,
+			listKeys.toggleStatusBar,
+			listKeys.togglePagination,
+			listKeys.toggleHelpMenu,
+		}
+	}
+
+	return model{
+		list:          groceryList,
+		keys:          listKeys,
+		delegateKeys:  delegateKeys,
+		dbItemHandler: dbItemHandler,
+	}
 }
 
 func (m model) Init() tea.Cmd {
@@ -28,52 +120,68 @@ func (m model) Init() tea.Cmd {
 }
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmds []tea.Cmd
+
 	switch msg := msg.(type) {
-	case tea.KeyMsg:
-		if msg.String() == "ctrl+c" {
-			return m, tea.Quit
-		}
 	case tea.WindowSizeMsg:
-		h, v := docStyle.GetFrameSize()
+		h, v := appStyle.GetFrameSize()
 		m.list.SetSize(msg.Width-h, msg.Height-v)
+
+	case tea.KeyMsg:
+		// Don't match any of the keys below if we're actively filtering.
+		if m.list.FilterState() == list.Filtering {
+			break
+		}
+
+		switch {
+		case key.Matches(msg, m.keys.toggleSpinner):
+			cmd := m.list.ToggleSpinner()
+			return m, cmd
+
+		case key.Matches(msg, m.keys.toggleTitleBar):
+			v := !m.list.ShowTitle()
+			m.list.SetShowTitle(v)
+			m.list.SetShowFilter(v)
+			m.list.SetFilteringEnabled(v)
+			return m, nil
+
+		case key.Matches(msg, m.keys.toggleStatusBar):
+			m.list.SetShowStatusBar(!m.list.ShowStatusBar())
+			return m, nil
+
+		case key.Matches(msg, m.keys.togglePagination):
+			m.list.SetShowPagination(!m.list.ShowPagination())
+			return m, nil
+
+		case key.Matches(msg, m.keys.toggleHelpMenu):
+			m.list.SetShowHelp(!m.list.ShowHelp())
+			return m, nil
+
+		case key.Matches(msg, m.keys.insertItem):
+			m.delegateKeys.remove.SetEnabled(true)
+			newItem := m.dbItemHandler.GetNextQuote()
+			insCmd := m.list.InsertItem(0, newItem)
+			statusCmd := m.list.NewStatusMessage(statusMessageStyle("Added " + newItem.Title()))
+			return m, tea.Batch(insCmd, statusCmd)
+		}
 	}
 
-	var cmd tea.Cmd
-	m.list, cmd = m.list.Update(msg)
-	return m, cmd
+	// This will also call our delegate's update function.
+	newListModel, cmd := m.list.Update(msg)
+	m.list = newListModel
+	cmds = append(cmds, cmd)
+
+	return m, tea.Batch(cmds...)
 }
 
 func (m model) View() string {
-	return docStyle.Render(m.list.View())
+	return appStyle.Render(m.list.View())
 }
 
 func main() {
-	// Initialize Database.
-	db, err := InitDb()
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
+	//rand.Seed(time.Now().UTC().UnixNano())
 
-	// Get splice of items from database.
-	quotes, err := GetAllQuotes(db)
-	if err != nil {
-	    fmt.Println(err)
-	    os.Exit(1)
-	}
-
-	items := []list.Item{}
-
-	for _, quote := range quotes {
-        items = append(items, quote)
-	}
-
-	m := model{list: list.New(items, list.NewDefaultDelegate(), 0, 0)}
-	m.list.Title = "Quotes"
-
-	p := tea.NewProgram(m, tea.WithAltScreen())
-
-	if _, err := p.Run(); err != nil {
+	if _, err := tea.NewProgram(newModel(), tea.WithAltScreen()).Run(); err != nil {
 		fmt.Println("Error running program:", err)
 		os.Exit(1)
 	}
